9dffd5a9a153de4b805c4ba6f5754a4ceab27f48
Mehmet Emin Kartal <mehmet@appac.ltd>
2022-06-26 03:41:29 +0300

    Eslint Kurulumu (#8)

--- tools/get_cf_url.js
+++ tools/get_cf_url.js
@@ -1,26 +1,30 @@
-import fetch from 'node-fetch';
-
+import fetch from "node-fetch";
 
 // commit hash from args
 const accountId = process.argv[3];
 const apiKey = process.argv[2];
 const commitHash = process.argv[4];
 
-fetch(`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/appaccommerce/deployments`, {
-  headers: {
-    "Authorization": `Bearer ${apiKey}`,
-    "Content-Type": "application/json"
+fetch(
+  `https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/appaccommerce/deployments`,
+  {
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
   }
-})
-.then(response => response.json())
-.then(data => {
-  console.log("Finding ", commitHash);
-  const build = data.result.find(build => build.deployment_trigger.metadata.commit_hash === commitHash);
-  if (build) {
-    console.log(build.url);
-    console.log(`::set-output name=url::${build.url}`);
-  } else {
-    console.error("Build Not Found");
-    process.exit(-1);
-  }
-});
+)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Finding ", commitHash);
+    const build = data.result.find(
+      (build) => build.deployment_trigger.metadata.commit_hash === commitHash
+    );
+    if (build) {
+      console.log(build.url);
+      console.log(`::set-output name=url::${build.url}`);
+    } else {
+      console.error("Build Not Found");
+      process.exit(-1);
+    }
+  });
